4d86a1c534bace4c71d0e35762ec85e116479fd7
chensong <chensongbj25@gamil.com>
2022-11-11 14:46:53 +0800

    [rtc_player]

---
+++ Server/rtc_player/cwebscoket_connection.h
@@ -0,0 +1,13 @@
+/***********************************************************************************************
+created: 		2022-01-20
+
+author:			chensong
+
+purpose:		assertion macros
+************************************************************************************************/
+#ifndef C_WEBSOCKET_CONNECTION_H
+#define C_WEBSOCKET_CONNECTION_H
+namespace chen {
+
+}
+#endif // !C_WEBSOCKET_CONNECTION_H
---
+++ Server/rtc_player/cwebsocket_protoo.h
@@ -0,0 +1,17 @@
+/***********************************************************************************************
+created: 		2022-01-20
+
+author:			chensong
+
+purpose:		assertion macros
+************************************************************************************************/
+#ifndef C_WEBSOCKET_PROTOO_H_
+#define C_WEBSOCKET_PROTOO_H_
+
+namespace chen {
+	namespace websocket_protoo
+	{
+
+	}
+}
+#endif // !C_WEBSOCKET_PROTOO_H_
