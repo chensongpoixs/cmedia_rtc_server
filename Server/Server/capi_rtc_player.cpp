4c002b344d30f64f914ec6e0c546b356132eb934
chensong <15850774503@163.com>
2023-02-10 16:53:53 +0800

    [增加接口]

--- Server/Server/capi_rtc_player.cpp
+++ Server/Server/capi_rtc_player.cpp
@@ -10,5 +10,27 @@ purpose:		api_rtc_publish
 #include "capi_rtc_player.h"
 
 namespace chen {
-
+	capi_rtc_player::~capi_rtc_player()
+	{
+	}
+	bool capi_rtc_player::_negotiate_play_capability()
+	{
+		return true;
+	}
+	bool capi_rtc_player::_generate_play_local_sdp(crtc_sdp & local_sdp, crtc_source_description * stream_desc, bool unified_plan, bool audio_before_video)
+	{
+		return true;
+	}
+	bool capi_rtc_player::_generate_play_local_sdp_for_audio(crtc_sdp & local_sdp, crtc_source_description * stream_desc, std::string cname)
+	{
+		return true;
+	}
+	bool capi_rtc_player::_generate_play_local_sdp_for_video(crtc_sdp & local_sdp, crtc_source_description * stream_desc, bool unified_plan, std::string cname)
+	{
+		return true;
+	}
+	bool capi_rtc_player::_create_player(std::map<uint32, crtc_track_description*> sub_relations)
+	{
+		return true;
+	}
 }
--- Server/Server/capi_rtc_player.h
+++ Server/Server/capi_rtc_player.h
@@ -14,12 +14,25 @@ purpose:		api_rtc_publish
 #include <iostream>
 #include <vector>
 #include <map>
+#include "crtc_source_description.h"
+#include "crtc_track_description.h"
 namespace chen {
 	class capi_rtc_player
 	{
 	public:
-		capi_rtc_player() {}
-		~capi_rtc_player() {}
+		explicit capi_rtc_player() {}
+		virtual ~capi_rtc_player();
+
+
+
+	private:
+		//TODO: Use StreamDescription to negotiate and remove first negotiate_play_capability function
+		bool _negotiate_play_capability(   );
+		bool _generate_play_local_sdp(  crtc_sdp& local_sdp, crtc_source_description* stream_desc, bool unified_plan, bool audio_before_video);
+		bool _generate_play_local_sdp_for_audio(crtc_sdp& local_sdp, crtc_source_description* stream_desc, std::string cname);
+		bool _generate_play_local_sdp_for_video(crtc_sdp& local_sdp, crtc_source_description* stream_desc, bool unified_plan, std::string cname);
+		bool _create_player(  std::map<uint32, crtc_track_description*> sub_relations);
+		//bool _create_publisher(crtc_source_description* stream_desc);
 	protected:
 	private:
 	};
