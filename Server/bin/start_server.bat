85d5859cb00b481d0368d577560a7b1110a30ecc
chensong <15850774503@163.com>
2023-05-12 18:56:51 +0800

    [rtsp]

--- Server/bin/start_server.bat
+++ Server/bin/start_server.bat
@@ -2,13 +2,13 @@
 
 setlocal
 
-set para=x64\Debug
+set para=x64\Release
 
 if "%1" NEQ "" set para=%1
 
 set Dir=%~dp0%para%\
 
 @ping -n 1 127.0.0.1>nul
-start "media_rtc_server" /D %Dir% %Dir%Server.exe ..\..\server.cfg  ..\..\log
+start "media_rtc_server" /D %Dir% %Dir%media_rtc_server.exe ..\..\server.cfg  ..\..\log
 
 REM pause
